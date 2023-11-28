52100c592bf79b0a44321d31ffbb357d6d3012c9
Tyler <tyler@propeldata.com>
2023-11-28 04:44:11 -0600

    fix: add lint script and configure linters for js and ts

--- javascript/test.js
+++ javascript/test.js
@@ -1,12 +1,12 @@
 // token.test.js
 
-const { getToken } = require("./getToken"); // replace './getToken' with the path to your getToken function
+const { getToken } = require('./getToken'); // replace './getToken' with the path to your getToken function
 
-describe("Token Fetch Test", () => {
-  it("should fetch a token", async () => {
+describe('Token Fetch Test', () => {
+  it('should fetch a token', async () => {
     const token = await getToken();
     console.log(token);
     expect(token).toBeTruthy();
-    expect(typeof token).toBe("string");
+    expect(typeof token).toBe('string');
   });
 });
--- typescript/src/test.ts
+++ typescript/src/test.ts
@@ -1,13 +1,11 @@
-import { getAccessToken } from './getToken';
-import { getEnvVariable } from './utils';
+import { getAccessToken } from './getToken'
+import { getEnvVariable } from './utils'
 
 describe('OAuth Token Retrieval', () => {
   it('retrieves an access token', async () => {
-    const token = await getAccessToken(getEnvVariable('CLIENT_ID'), getEnvVariable('CLIENT_SECRET'), getEnvVariable('TOKEN_HOST'), getEnvVariable('TOKEN_PATH'));
-    expect(token).toBeDefined();
-    expect(token.access_token).toBeDefined();
-    console.log(token.access_token);
-  });
-});
-
-
+    const token = await getAccessToken(getEnvVariable('CLIENT_ID'), getEnvVariable('CLIENT_SECRET'), getEnvVariable('TOKEN_HOST'), getEnvVariable('TOKEN_PATH'))
+    expect(token).toBeDefined()
+    expect(token.access_token).toBeDefined()
+    console.log(token.access_token)
+  })
+})
